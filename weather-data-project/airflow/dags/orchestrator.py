a7b12c7b7d67efe3c7d309cb09ba0e1a1eb36d35
Joel <atchj001@gmail.com>
2026-01-19 15:20:23 +0000

    shedule time update

--- weather-data-project/airflow/dags/orchestrator.py
+++ weather-data-project/airflow/dags/orchestrator.py
@@ -22,7 +22,7 @@ default_args = {
 dag = DAG(
     dag_id='weather-api-orchestrator-dbt-orchestrator',
     default_args=default_args,
-    schedule = timedelta(minutes=1)
+    schedule = timedelta(minutes=10)
 )
 
 with dag:
